d095dd20614867190380ffa3f4cc22a6e4a2998a
Wiktoria Wydra <s30543@pjwstk.edu.pl>
2025-03-05 18:34:33 +0100

    Modification 3

--- cw1/cw1/Program.cs
+++ cw1/cw1/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
 Console.WriteLine("Hello, World!");
 
-int a = 10;
-int b = 5;
-Console.WriteLine(a + b);
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
